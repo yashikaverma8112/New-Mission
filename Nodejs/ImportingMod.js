7334f6c67f13da53cb7e184e2ba4796e6c0829cf
yashikaverma8112 <yashikaverma8112@gmail.com>
2025-07-04 13:52:48 +0530

    changing in moduleRev,ImportingMod

---
+++ Nodejs/ImportingMod.js
@@ -0,0 +1,6 @@
+const learnNodejs = require('./ModuleRev.js');
+console.log(learnNodejs.name);
+
+learnNodejs.sum(2,5);
+
+console.log(learnNodejs.indirectExportValue);
--- Nodejs/ModuleRev.js
+++ Nodejs/ModuleRev.js
@@ -1,1 +1,13 @@
+module.exports={
+    name:'Learn nodejs',
+    sum:function(a,b){
+    console.log(a+b);
+}}
+
+
+module.exports.indirectExport = function(){
+    console.log("function of indirect export");
+}
+module.exports.indirectExportValue = 123;
+
 

d5ed2c3a586d9e2c9a404c8a9bdb56f78d0c9f01
Yashika Verma <96378034+yashikaverma8112@users.noreply.github.com>
2025-07-06 12:46:56 +0530

    Create CountSetBit.cpp

---
+++ DSA/CountSetBit.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int setBits(int n) {
+        int ans = 0;
+        while(n){
+            ans += n & 1;
+            
+            n = n>> 1;
+        }
+        return ans;
+    }
+};

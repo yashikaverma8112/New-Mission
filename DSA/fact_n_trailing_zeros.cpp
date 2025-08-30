a5de7365970cd58eb201672ce93a6ea9d959d0db
Yashika Verma <96378034+yashikaverma8112@users.noreply.github.com>
2025-08-30 13:27:53 +0530

    Update fact_n_trailing_zeros.cpp

--- DSA/fact_n_trailing_zeros.cpp
+++ DSA/fact_n_trailing_zeros.cpp
@@ -3,7 +3,26 @@
 class Solution {
   public:
     int findNum(int n) {
-        // code here
+        /* TC - O(nlogn)
+
+        int findNum(int n)
+        {
+        int dem1=0;  
+
+         for(int i=5;i<=n*5;i=i+5) { 
+             int i1=i; 
+                 while(i1%5==0 ) {  
+                     dem1++; 
+                     i1=i1/5; 
+                  if(dem1==n) return i;
+
+                 }
+         }
+        }
+*/
+
+
+      // TC O(logn logn)
         int low = 0, high = 5*n, ans = -1;
         
         while(low<=high){

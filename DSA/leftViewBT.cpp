fc86180295e52cf5ac164b3eb4f55499e8ef6d24
Yashika Verma <96378034+yashikaverma8112@users.noreply.github.com>
2025-09-08 19:52:05 +0530

    Create leftViewBT.cpp

---
+++ DSA/leftViewBT.cpp
@@ -0,0 +1,49 @@
+/*
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+ */
+
+class Solution {
+  public:
+    vector<int> leftView(Node *root) {
+        if(root==NULL){
+            return {};
+        }
+        
+        vector<int>ans;
+        
+        queue<Node*>q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int n = q.size();
+            
+            for(int i=0; i<n; i++){
+                Node *top = q.front();
+                q.pop();
+                
+                if(i==0){
+                    ans.push_back(top->data);
+                }
+                if(top->left){
+                    q.push(top->left);
+                }
+                if(top->right){
+                    q.push(top->right);
+                }
+            }
+        }
+        return ans;
+    }
+};

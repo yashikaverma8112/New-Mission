e3e9855842229c290858006c41d10e7a4ff2ca99
Yashika Verma <96378034+yashikaverma8112@users.noreply.github.com>
2025-09-09 17:43:37 +0530

    Create buy-and-sell-a-share-at-most-twice.cpp

---
+++ DSA/buy-and-sell-a-share-at-most-twice.cpp
@@ -0,0 +1,19 @@
+// https://www.geeksforgeeks.org/problems/buy-and-sell-a-share-at-most-twice/1
+
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        int buy1 = INT_MAX;
+        int buy2 = INT_MAX;
+        int sell1 = INT_MIN;
+        int sell2 = INT_MIN;
+        
+        for(int i=0; i<prices.size();i++){
+            buy1 = min(buy1,prices[i]);
+            sell1 = max(sell1, prices[i]-buy1);
+            buy2 = min(buy2,prices[i]-sell1);
+            sell2 = max(sell2,prices[i]-buy2);
+        }
+        return sell2;
+    }
+};

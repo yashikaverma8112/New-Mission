af06a038304b288d53a802fff4087464a57af6a7
Yashika Verma <96378034+yashikaverma8112@users.noreply.github.com>
2025-10-03 11:26:21 +0530

    Update and rename minValue.cpp to minValueBST.cpp

--- DSA/minValueBST.cpp
+++ DSA/minValueBST.cpp
@@ -16,17 +16,34 @@ Node* newNode(int val) {
 }
 */
 
+// Function to find the minimum element in the given BST.
+
+/* struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+*/
+
 class Solution {
   public:
     int minValue(Node* root) {
         
-        int mini = INT_MAX;
         if(root==NULL){
-            return mini;
+            return -1;
         }
-        mini = root->data;
+       int mini = root->data;
         if(root->left){
-            mini = min(mini, minValue(root->left));
+            mini =  minValue(root->left);
         }
         
         return mini;

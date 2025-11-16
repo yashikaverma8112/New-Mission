834f60a80d81330bcbb36441e771bde858505de8
Yashika Verma <96378034+yashikaverma8112@users.noreply.github.com>
2025-11-16 17:28:25 +0530

    Create nextPermutation.cpp

---
+++ DSA/nextPermutation/nextPermutation.cpp
@@ -0,0 +1,28 @@
+//https://www.geeksforgeeks.org/problems/next-permutation5226/1
+
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        int n = arr.size();
+        int ind = -1;
+        for(int i=n-2; i>=0; i--){
+            if(arr[i] < arr[i+1]){
+                ind = i;
+                break;
+            }
+        }
+        
+        if(ind == -1){
+            reverse(arr.begin(),arr.end());
+            return;
+        }
+        
+        for(int i=n-1; i>=ind; i--){
+            if(arr[i] > arr[ind]){
+                swap(arr[i],arr[ind]);
+                break;
+            }
+        }
+        reverse(arr.begin()+ind+1, arr.end());
+    }
+};
